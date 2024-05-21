fcf6331e48747d5aad0d1c86a010f3a4ca5e4b72
lauramarcospel <lauramarcosces@gmail.com>
2024-05-21 14:09:01 +0200

    cambios2

---
+++ components/ProductDetail.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="grid grid-cols-2">
+        <div>
+            <img :src="Product.image" alt="">
+         </div>
+         <div>
+        <h2>{{ product.title }}</h2>
+        <p>{{ product.price }}</p>
+        <h3>{{ product.description }}</h3>
+    </div>
+    </div>
+   
+</template>
+
+<script setup>
+const { product } = defineProps(["product"])
+</script>
+
+<style scoped>
+
+</style>
